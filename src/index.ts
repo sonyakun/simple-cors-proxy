e747225b438ed0c1d12ea6c5248b766a14ae633d
sonyakun <90973770+sonyakun@users.noreply.github.com>
2025-06-23 00:46:39 +0900

    fix: cors

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,18 @@ app.get(
 
 app.get('/connect/serverlist.php', async (c) => {
   const json = await ky.get('https://api.zpw.jp/connect/serverlist.php').json();
+  c.header("Access-Control-Allow-Origin", "*");
+  c.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  c.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // @ts-ignore
   return c.json(json);
 })
 
 app.get('/connect/v2/serverlist.php', async (c) => {
   const json = await ky.get('https://api.zpw.jp/connect/serverlist.php').json();
+  c.header("Access-Control-Allow-Origin", "*");
+  c.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  c.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   // @ts-ignore
   return c.json(json);
 })
